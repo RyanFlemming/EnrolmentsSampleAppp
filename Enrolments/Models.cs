aa670f35c1be18da943e89ff2ebb3e7cab038ec2
Ryan <rmf919@live.com>
2016-10-21 13:32:06 +1100

    initial commit

---
+++ Enrolments/Models.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enrolments
+{
+	internal enum DegreeType
+	{
+		Associate, Bachelor, Master, Doctorate
+	}
+
+	internal abstract class Person
+	{
+		private string _firstName;
+		private string _lastName;
+
+		public Person(string firstName, string lastName)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+		}
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+	}
+	internal class Student : Person
+	{
+		private Stack<int> _grades;
+
+		public Stack<int> Grades
+		{
+			get { return _grades; }
+			set { _grades = value; }
+		}
+
+		// Calling the base constructor, in order to set first & lastname
+		public Student(string firstName, string lastName)
+			: base(firstName, lastName)
+		{
+			_grades = new Stack<int>();
+		}
+	}
+
+	internal class Teacher : Person
+	{
+		public Teacher(string firstName, string lastName)
+			: base(firstName, lastName)
+		{
+		}
+		public override string ToString()
+		{
+			return string.Format("{0} {1}", FirstName, LastName);
+		}
+	}
+
+	internal class Course
+	{
+		private List<Student> _students;
+		private Teacher _teachers;
+		private string _name;
+
+		internal List<Student> Students
+		{
+			get { return _students; }
+			set { _students = value; }
+		}
+		
+		internal Teacher Teachers
+		{
+			get { return _teachers; }
+			set { _teachers = value; }
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		internal Course(string name, List<Student> students, Teacher teachers)
+		{
+			_name = name;
+			_students = students;
+			_teachers = teachers;
+		}
+
+		public void ListStudents()
+		{
+			var h = this.Students.Cast<string>().OrderBy(item => int.Parse(item));
+			foreach (Student s in this._students)
+			{
+				Console.WriteLine("First Name: {0}\nLast Name: {1}\n", s.FirstName, s.LastName);
+			}
+		}
+	}
+
+	internal class Degree
+	{
+		private string courseName;
+		private DegreeType levelOfDegree;
+		private List<Course> courses;
+
+		public string Name
+		{
+			get { return courseName; }
+			set { courseName = value; }
+		}
+
+		internal List<Course> Courses
+		{
+			get { return courses; }
+			set { courses = value; }
+		}
+
+		internal DegreeType LevelOfDegree
+		{
+			get { return levelOfDegree; }
+			set { levelOfDegree = value; }
+		}
+
+		internal Degree(string name, DegreeType degreeType, List<Course> coursesChosen)
+		{
+			this.courseName = name;
+			this.levelOfDegree = degreeType;
+			this.courses = coursesChosen;
+		}
+
+		internal Degree(string name, DegreeType degreeType)
+			: this(name, degreeType, new List<Course>())
+		{
+		}
+	}
+
+	internal class UProgram
+	{
+		private string _name;
+		private List<Degree> _degrees;
+
+		internal List<Degree> Degrees
+		{
+			get { return _degrees; }
+			set { _degrees = value; }
+		}
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		internal UProgram(string courseName, List<Degree> degrees)
+		{
+			this._name = courseName;
+			this._degrees = degrees;
+		}
+
+		internal UProgram(string courseName)
+			: this(courseName, new List<Degree>())
+		{
+		}
+	}
+}
---
+++ Enrolments/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enrolments
+{
+	class Program
+	{	
+		static void Main(string[] args)
+		{
+			// Initialising 3 student objects and adding them to our generic List
+			List<Student> studentCollection = new List<Student>();
+			studentCollection.Add(new Student("Ashna", "C."));
+			studentCollection.Add(new Student("Ryan", "F."));
+			studentCollection.Add(new Student("That", "Other Guy"));
+
+			Teacher[] teachers = { new Teacher("Nick", "A.") };
+
+			Course course = new Course("Programming With C#", studentCollection, teachers[0]);
+
+			Degree degree = new Degree("ASP.NET MVC With Entity Framework", DegreeType.Bachelor);
+			degree.Courses.Add(course);
+
+			UProgram program = new UProgram("IT");
+			program.Degrees.Add(degree);
+
+			PrintStudentInfo(program, studentCollection);
+			AssignGrades(studentCollection);
+			ShowGrades(studentCollection);
+
+			Console.ReadKey();
+		}
+		// Making sure students have random grades
+		private static readonly Random randomGrade = new Random(5);
+
+		static void AssignGrades(List<Student> studentCollection)
+		{
+
+			foreach (var student in studentCollection)
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					student.Grades.Push(randomGrade.Next(60, 100));
+				}
+			}
+		}
+
+
+		private static void ShowGrades(List<Student> studentCollection)
+		{
+			foreach (var student in studentCollection)
+			{
+				Console.WriteLine("{0} {1} has the following grades: \n", student.FirstName, student.LastName);
+				foreach (var grade in student.Grades)
+				{
+					Console.WriteLine(grade);
+				}
+				Console.WriteLine();
+			}
+		}
+
+		internal static void PrintStudentInfo(UProgram program, List<Student> students)
+		{
+			var degrees = program.Degrees.First();
+			Console.WriteLine("The {0} program contains the {1} degree.\n", program.Name, degrees.Name);
+
+			var course = degrees.Courses.First();
+			Console.WriteLine("The {0} degree contains the course {1} and is taught by {2}\n",
+				degrees.Name, course.Name, course.Teachers.ToString());
+			Console.WriteLine("The {0} course has {1} students:\n", course.Name, students.Count);
+
+			course.ListStudents();
+		}
+	}
+}
